40fcd92cddac704bcebbc8790bf19029aa6ca2d9
Craig Champion <craig@isai.co.uk>
2021-05-29 09:46:17 +0100

    Updates

--- Lessons/ISAI.Lessons.Mobile/ISAI.Lessons.Mobile/App.xaml.cs
+++ Lessons/ISAI.Lessons.Mobile/ISAI.Lessons.Mobile/App.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using ISAI.Lessons.Core.Services;
 using ISAI.Lessons.Mobile.Services;
 using ISAI.Lessons.Models.Interfaces;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 using System;
 using System.Collections.Generic;
 using Xamarin.Forms;
@@ -23,6 +26,10 @@ namespace ISAI.Lessons.Mobile
         protected  override async void OnStart()
         {
             await DependencyService.Get<ISqliteService>().InitializeAsync();
+
+            AppCenter.Start("ios=0965cd30-5ffc-43db-932e-81a80aad6007;" +
+                  "android=9c21c892-f0b6-471a-830b-c361277385c6;",
+                  typeof(Analytics), typeof(Crashes));
         }
 
         protected override void OnSleep()
