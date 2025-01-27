8fd66ef8e3632dccad7bf69bd7c1230e13540089
Craig Champion <craig@isai.co.uk>
2025-01-27 17:10:18 +0000

    Mobile project re-organsiation

--- Lessons/ISAI.Lesson.Mobile/ISAI.Lessons.Mobile/Platforms/iOS/AppDelegate.cs
+++ Lessons/ISAI.Lesson.Mobile/ISAI.Lessons.Mobile/Platforms/iOS/AppDelegate.cs
@@ -1,4 +1,7 @@
 ﻿using Foundation;
+using ISAI.Lessons.Mobile.Maui.Platforms.iOS.Helpers;
+using ISAI.Lessons.Models.Interfaces;
+using ISAI.Lessons.Mobile.Helper;
 
 namespace ISAI.Lessons.Mobile;
 
@@ -6,4 +9,11 @@ namespace ISAI.Lessons.Mobile;
 public class AppDelegate : MauiUIApplicationDelegate
 {
     protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    
+    public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+    {
+        DependencyService.Register<IHud, Hud>();
+        return base.FinishedLaunching(app, options);
+    }
+    
 }
